321eac73b17c3066a6972f249130fca9f04ee994
Karthik Raja <Karthikraja.kalaimani@syncfusion.com>
2020-03-26 19:01:33 +0530

    How to show the PopupView at relative position with NavigationPage in iPhoneX committed

---
+++ Popup_Demo/Popup_Demo/Popup_Demo/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Syncfusion.XForms.PopupLayout;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms;
+
+namespace Popup_Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            On<iOS>().SetUseSafeArea(true);
+           
+        }
+        private void Button_Clicked1(object sender, EventArgs e)
+        {
+            PopupLayout.ShowRelativeToView(btn, RelativePosition.AlignBottom, 0, 0);
+        }
+    }
+}
